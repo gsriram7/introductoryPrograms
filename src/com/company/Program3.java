e1eb72e7e4ba2276b6cc3232157f6dfe10006afa
gsriram7 <gsriram7@gmail.com>
2015-01-10 09:33:46 +0530

    third program

---
+++ src/com/company/Program3.java
@@ -0,0 +1,13 @@
+package com.company;
+
+/**
+ * Created by selvaram on 1/10/15.
+ */
+public class Program3 {
+
+    public static void printVertical(int n){
+        for(int i=0;i<n;i++){
+            System.out.println("*");
+        }
+    }
+}
